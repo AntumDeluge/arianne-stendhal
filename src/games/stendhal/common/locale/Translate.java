55cf34cd91d3d447ec613e75104772c90faceedc
Jordan Irwin <antumdeluge@gmail.com>
2022-01-05 11:42:05 -0800

    Beginnings of a localization interface

---
+++ src/games/stendhal/common/locale/Translate.java
@@ -0,0 +1,20 @@
+/***************************************************************************
+ *                      (C) Copyright 2022 - Arianne                       *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.common.locale;
+
+
+public class Translate {
+
+	public static String T(final String s) {
+		return s;
+	}
+}
